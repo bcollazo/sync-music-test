7ef76b5a150ee597e1208d789bd220518e26d37d
Bryan Collazo <bcollazo@mit.edu>
2014-02-09 21:31:57 -0500

    backend

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,4 +3,37 @@ class MainController < ApplicationController
 	def index
 	end
 
+
+	def request
+		ip = request.remote_ip
+		time = params[:time]
+		d = Time.new
+
+		x = User.find_or_create_by_ip(ip)
+		x.datetime = time
+		x.save
+
+		done = false
+		while User.count != 2
+			puts "+++++"
+			puts User.all
+		end
+
+		a = User.all[0].datetime
+		b = User.all[1].datetime
+
+		offset = 5
+
+		c = Time.new
+		
+		response = c + " " + d
+
+		# calculate
+		render :json => response.to_json
+	end
+
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ SyncMusicTest::Application.routes.draw do
 
 	root :to => "main#index"
 
+	post "/request" => "main#request"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
