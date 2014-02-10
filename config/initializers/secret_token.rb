2f49001941ba2d1b69fa1a240c8397cc81855f8a
Bryan Collazo <bcollazo@mit.edu>
2014-02-09 20:59:52 -0500

    inital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SyncMusicTest::Application.config.secret_key_base = 'c9981123c0146eba1cc00f64943f3e8c3c1614bf887486be09bc4342a73b3214fd7f5155111b2556a4005753ce73f91cdf2116e1cff0a2d4b2d14fbe98c97d48'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SyncMusicTest::Application.config.session_store :cookie_store, key: '_sync-music-test_session'
